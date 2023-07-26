b923ec6cffd40b5b21413899ac13cad80cd9c211
DanHT22 <tolumes.daniela@gmail.com>
2023-07-26 14:11:41 -0600

    Ejercicio de control de flujo con JAVa

---
+++ src/ImprimirNombre.java
@@ -0,0 +1,6 @@
+
+public class ImprimirNombre {
+	public static void main(String[] args) {
+		System.out.println("Daniela Heredia Tolumes");
+	}
+}
---
+++ src/comissionCalculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class comissionCalculator {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		System.out.print("Escirbe el total del ticket: ");
+		float ticket = in.nextFloat();
+		char comision = 0; 
+		float total = 0F;
+		if (ticket > 10000) {
+			comision = '3' ;
+			total = ticket * 1.3F;
+		}else if (ticket>5000 && ticket<9999 ){
+			comision='2';
+			total = ticket * 2.3F;
+		}else if (ticket>1000 && ticket<4999 ){
+			comision='1';
+			total = ticket * 1.3F;
+		}else if (ticket>1000 ){
+			comision='0';
+			total = ticket;
+		}
+		System.out.println("comision = " + total);
+		in.close();
+	}
+
+}
