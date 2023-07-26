d7ad0a660e6db79de92ff35419065842e4a1e1f8
Daniela Heredia T <70339598+DanHT22@users.noreply.github.com>
2023-07-26 16:22:48 -0600

    correcciones de operadores <=

--- src/comissionCalculator.java
+++ src/comissionCalculator.java
@@ -8,16 +8,16 @@ public class comissionCalculator {
 		float ticket = in.nextFloat();
 		char comision = 0; 
 		float total = 0F;
-		if (ticket > 10000) {
+		if (ticket >= 10000) {
 			comision = '3' ;
 			total = ticket * 0.3F;
-		}else if (ticket>5000 && ticket<9999 ){
+		}else if (ticket >= 5000 && ticket<=9999 ){
 			comision='2';
 			total = ticket * 0.2F;
-		}else if (ticket>1000 && ticket<4999 ){
+		}else if (ticket >= 1000 && ticket<=4999 ){
 			comision='1';
 			total = ticket * 0.1F;
-		}else if (ticket>1000 ){
+		}else if (ticket >= 1000 ){
 			comision='0';
 			total = ticket;
 		}
